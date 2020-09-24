4b49010a6048fbdfd189ce814647232212aab8e9
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2020-09-24 15:44:24 +0300

    Calculate Weekly Pay
    Calculate Weekly Pay with taxes

---
+++ CalculateWeeklyPay/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PAYRATE 12.00
+#define TAXRATE_300 .15
+#define TAXRATE_150 .20
+#define TAXRATE_REST .25
+#define OVERTIME 40
+int main()
+{
+    //declare variables
+    int hours=0;
+    double grossPay=0.0;  //brüt ödemem
+    double taxes=0.0;        //vergiler
+    double netPay=0.0;      //net ödeme
+    printf("Please enter the number of hors worked ths week: ");
+    scanf("%d",&hours);
+    if(hours<=40)
+        grossPay=hours*PAYRATE;
+
+else{
+
+    grossPay=40*PAYRATE;
+    double overTimePay=(hours-40)*(PAYRATE*1.5);
+    grossPay+=overTimePay;
+}
+//calculate taxes
+if(grossPay<=300){
+     taxes=grossPay*TAXRATE_300;
+}
+else if(grossPay>300 && grossPay<=450){
+    taxes=300*TAXRATE_300;
+    taxes+=(grossPay-300)*TAXRATE_150;
+}
+else if(grossPay>450){
+    taxes=300*TAXRATE_300;
+    taxes+=150*TAXRATE_150;
+    taxes+=(grossPay-450)*TAXRATE_REST;
+}
+    //calculate net pay
+   netPay=grossPay-taxes;
+
+   //display output
+printf("Your gross pay this week is : %.2f\n",grossPay);
+printf("Your taxes this week is : %.2f\n",taxes);
+printf("Your net pay this week is : %.2f\n",netPay);
+    return 0;
+}
